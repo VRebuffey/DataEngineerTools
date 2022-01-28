3e7d5553b7ed73f0ae3d9ef1f5da849db35f8235
Valentin Rebuffey <valentin.rebuffey@edu.esiee.fr>
2022-01-28 17:39:37 +0100

    Exercices avant projet

--- 2Scrapy/citations_churchill_spider1.py
+++ 2Scrapy/citations_churchill_spider1.py
@@ -5,6 +5,9 @@ class ChurchillQuotesSpider(scrapy.Spider):
     start_urls = ["http://evene.lefigaro.fr/citations/winston-churchill",]
 
     def parse(self, response):
-        for cit in response.xpath('//div[@class="figsco__quote__text"]'):
+        for cit,auth in zip(response.xpath('//div[@class="figsco__quote__text"]'),
+                            response.xpath('//div[@class="figsco__fake__col-9"]')):
             text_value = cit.xpath('a/text()').extract_first()
-            yield { 'text' : text_value }
+            author_value = auth.xpath('a/text()').extract_first()
+            yield { 'text' : text_value.strip('“').strip('”'),
+                    'author' : author_value }         
